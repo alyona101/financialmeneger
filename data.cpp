00b4368d26aec4425448c957045dd00a75e0fef5
Eldar071 <eldar007nafikov@gmail.com>
2024-05-18 12:25:08 +0300

    My first commit

---
+++ data.cpp
@@ -0,0 +1,25 @@
+﻿//#include<iostream>
+//#include<string>
+using namespace std;
+namespace data
+{
+	struct Data
+	{
+		Data(string data)
+		{
+			string day = data.substr(0, 2);
+			string month = data.substr(3, 2);
+			string year = data.substr(6, 4);
+
+			day = stoi(day);
+			month = stoi(month);
+			year = stoi(year);
+		}
+
+		int day;
+		int month;
+		int year;
+	
+	};
+
+}
